3cdad3bf9de2de67b087408a23edf0b4fb123a60
andrey <andreymail26@yandex.ru>
2023-01-05 18:46:12 +0300

    Урок 3. Активити. Жизненный цикл. Старт.

--- app/src/main/java/ru/andrey/mycalculator/MainActivity.java
+++ app/src/main/java/ru/andrey/mycalculator/MainActivity.java
@@ -4,6 +4,7 @@ package ru.andrey.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
